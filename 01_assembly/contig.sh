12fe2ca1b09bca933ffb858c1ed8d7e730b175c3
xiongyi95 <147789870+xiongyi95@users.noreply.github.com>
2024-09-25 17:26:41 +0800

    contig.sh

--- 01_assembly/contig.sh
+++ 01_assembly/contig.sh
@@ -1,10 +1,15 @@
-#01 pacbio_hifi下机bam转fasta.gz
-bam2fasta hifi.bam -c 9 -o sample -j 20 &
+#!/bin/bash
 
-#02 hifisam组装基因组
- 
-hifiasm -o contig.asm   \
-        -t 72  \
-        ./sample.fasta
-        
-awk '/^S/{print ">"$2; print $3}' contig.asm.bp.p_ctg.gfa > contig.asm.fasta
+# Set paths for input and output directories
+HIFI_READS="path/to/hifi_reads.fastq"  # Path to Pacbio HiFi reads
+OUTPUT_DIR="assembly_output"  # Output directory for assembly results
+ASSEMBLY_PREFIX="assembly"  # Prefix for assembly output files
+
+# Create output directory
+mkdir -p ${OUTPUT_DIR}
+
+#  De novo assembly with Hifiasm
+hifiasm -o ${OUTPUT_DIR}/${ASSEMBLY_PREFIX} -t 16 ${HIFI_READS}
+
+# Convert GFA to FASTA (primary contigs)
+awk '$1 ~/S/{print ">"$2"\n"$3}' ${OUTPUT_DIR}/${ASSEMBLY_PREFIX}.bp.hap1.p_ctg.gfa > ${OUTPUT_DIR}/${ASSEMBLY_PREFIX}.primary_contigs.fasta
